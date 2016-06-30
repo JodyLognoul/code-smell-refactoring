53a00bed688e27ad7cef48bc152445290f31ab34
Jody Lognoul <lognoulj@gmail.com>
2016-06-30 15:00:40 +0200

    Long Method

---
+++ Bloaters/long-method.php
@@ -0,0 +1,59 @@
+<?php
+namespace Bloaters;
+
+use PHPUnit\Framework\TestCase;
+
+class Box {
+    public function __construct($unitCost, $height, $width)
+    {
+        $this->unitCost = $unitCost;
+        $this->height = $height;
+        $this->width = $width;
+    }
+
+    public function serializeCost()
+    {
+        // Calculate the cost.
+        $cost = $this->height * $this->width * $this->unitCost;
+
+        // print the cost.
+        return json_encode(["cost" => $cost]);
+    }
+}
+
+class RefactoredBox {
+
+    public function __construct($unitCost, $height, $width)
+    {
+        $this->unitCost = $unitCost;
+        $this->height = $height;
+        $this->width = $width;
+    }
+
+    public function serializeCost()
+    {
+        return $this->serialize(["cost" => $this->calculateCost()]);
+    }
+
+    protected function calculateCost()
+    {
+        return $this->height * $this->width * $this->unitCost;
+    }
+
+    protected function serialize($cost)
+    {
+        return json_encode($cost);
+    }
+
+}
+
+Class BoxTest extends TestCase {
+    public function testPrintCost()
+    {
+        $expectedResponse = '{"cost":140}';
+        $box = new Box(1.4, 10, 10);
+        $refactoredBox = new RefactoredBox(1.4, 10, 10);
+        $this->assertEquals($expectedResponse, $box->serializeCost());
+        $this->assertEquals($expectedResponse, $refactoredBox->serializeCost());
+    }
+}
